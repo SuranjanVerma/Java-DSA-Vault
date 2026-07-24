438db8f63f1662791250f6ab75e6f48813c4b163
Suranjan Kumar Verma <myidsuranjanverma@gmail.com>
2026-07-25 01:19:40 +0530

    Time: 3 ms (99.87%), Space: 46.6 MB (26.29%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,29 +1,18 @@
 class Solution {
     public String intToRoman(int num) {
 
-        TreeMap<Integer, String> map = new TreeMap<>();
-        
-        map.put(1000, "M");
-        map.put(900, "CM");
-        map.put(500, "D");
-        map.put(400, "CD");
-        map.put(100, "C");
-        map.put(90, "XC");
-        map.put(50, "L");
-        map.put(40, "XL");
-        map.put(10, "X");
-        map.put(9, "IX");
-        map.put(5, "V");
-        map.put(4, "IV");
-        map.put(1, "I");
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         
         StringBuilder roman = new StringBuilder();
-
-        while(num>0){
-            int closestValue = map.floorKey(num);
-            roman.append(map.get(closestValue));
-            num = num-closestValue;
+        
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                roman.append(symbols[i]);
+            }
         }
+        
         return roman.toString();
     }
 }
