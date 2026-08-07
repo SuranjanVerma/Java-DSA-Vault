808530ad6ec35f452799e9e4e4af8e3c5e1bc657
Suranjan Kumar Verma <myidsuranjanverma@gmail.com>
2026-08-07 21:12:32 +0530

    Time: 1 ms (77.27%), Space: 46.7 MB (44%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,6 +3,10 @@ class Solution {
         int l = 0;
         int n = nums.length;
 
+        if(n==0){
+            return 0;
+        }
+
         for(int r=1; r<n; r++){
             if(nums[l] != nums[r]){
                 l++;
