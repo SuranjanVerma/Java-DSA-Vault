68575fb19df56e029762ee633c92a30b89b9b8bb
Suranjan Kumar Verma <myidsuranjanverma@gmail.com>
2026-09-15 16:37:48 +0530

    Time: 0 ms (100%), Space: 47.9 MB (96.58%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void reverseString(char[] s) {
+
+        //Two pointer approach
+
+        int left = 0;
+        int right = s.length-1;
+
+        while(left<right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            
+            left++;
+            right--;
+        }
+
+    }
+}
